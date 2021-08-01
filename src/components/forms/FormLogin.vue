8df4778e3121f9b99dfaa7eda6963df9b734e7bf
Alairton Lima <alairtonlima3@gmail.com>
2021-08-01 17:08:34 -0300

    form login

--- src/components/forms/FormLogin.vue
+++ src/components/forms/FormLogin.vue
@@ -22,7 +22,14 @@
         />
       </div>
 
-      <ButtonLogin/>
+      <button
+      class="btn btn-primary"
+      id="button_login"
+      type="submit"
+      v-on:click="LoginVerification"
+    >
+      ENTRE EM SUA CONTA
+    </button>
       
       <div class="inline"></div>
      
@@ -30,11 +37,20 @@
 </template>
 
 <script>
-import ButtonLogin from '../forms/ButtoLogin.vue'
+
 export default {
   name: "FormLogin",
   components: {
-    ButtonLogin
+  },
+    methods: {
+    LoginVerification() {
+      let password = document.getElementById("password").value;
+      if (password.length > 6) {
+        return true;
+      } else {
+        alert("A senha precisa ter pelo menos seis caracteres");
+      }
+    }
   }
 };
 
@@ -73,6 +89,13 @@ h1, h6, a {
   padding: 0;
   height: 100%;
   background-color: #17a2b8;
+}
+#button_login {
+  position: relative;
+  top: 100px;
+  left: 30px;
+  width: 335px;
+  border-radius: 0%;
 }
 
 h6 {
